8b0506dc56b2899ceb42fd11515090c875a580de
Grant Forrest <gaforres@gmail.com>
2020-07-08 23:14:28 -0400

    add link to devlog

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { GifGrid } from './features/gifs/GifGrid';
-import { Container, makeStyles, Box, Typography } from '@material-ui/core';
+import {
+  Container,
+  makeStyles,
+  Box,
+  Typography,
+  Link,
+} from '@material-ui/core';
 import { GifSearch } from './features/gifs/GifSearch';
 import { GifLightbox } from './features/gifs/GifLightbox';
 
@@ -19,8 +25,11 @@ function App() {
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center" width="100%">
-      <Container component="header" maxWidth="md">
+      <Container component="header" maxWidth="md" style={{ marginBottom: 16 }}>
         <Typography variant="h1">Gifs!</Typography>
+        <Link href="https://github.com/a-type/signal-giphy#devlog">
+          Read the devlog for more detail!
+        </Link>
       </Container>
       <Container component="section" maxWidth="md">
         <GifSearch className={classes.search} fullWidth />
